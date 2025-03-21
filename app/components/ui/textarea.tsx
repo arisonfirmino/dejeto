b661aacd30867829454e0e4f5494a4058851d3a3
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-03-21 14:22:07 -0300

    style: adjust Textarea border opacity

--- app/components/ui/textarea.tsx
+++ app/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 aria-invalid:border-destructive flex field-sizing-content h-8 w-full resize-none rounded-xl border bg-transparent px-3 py-1.5 text-sm transition-[color,box-shadow] outline-none placeholder:lowercase focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 [&::-webkit-scrollbar]:hidden",
+        "border-input/30 placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 aria-invalid:border-destructive flex field-sizing-content h-8 w-full resize-none rounded-xl border bg-transparent px-3 py-1.5 text-sm transition-[color,box-shadow] outline-none placeholder:lowercase focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 [&::-webkit-scrollbar]:hidden",
         className,
       )}
       {...props}
