353ba7af6efc1d0817e662be766e89004a96088e
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-03-23 19:45:27 -0300

    refactor: order comments by creation date in getPosts function

--- app/helpers/getPosts.ts
+++ app/helpers/getPosts.ts
@@ -2,7 +2,11 @@ import { db } from "@/app/lib/prisma";
 
 export const getPosts = async () => {
   const posts = await db.post.findMany({
-    include: { user: true, likes: true, comments: { include: { user: true } } },
+    include: {
+      user: true,
+      likes: true,
+      comments: { include: { user: true }, orderBy: { created_at: "desc" } },
+    },
     orderBy: { created_at: "desc" },
   });
 
